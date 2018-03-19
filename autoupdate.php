395df090a7f9032d66643189429f560e5e82218f
Jason Prendergast <32454226+JasonPrendergast@users.noreply.github.com>
2018-03-19 11:25:12 +0000

    Auto updater
    First try auto updater

---
+++ autoupdate.php
@@ -0,0 +1,62 @@
+<?php
+
+/* 
+ * @package JasonPrendergast-plugin
+ */
+/*
+ * Plugin Name: JasonPrendergast-plugin
+ * Plugin URI: https://github.com/JasonPrendergast
+ * Description:First try at building an auto update class for WP in php  
+ * Version: 0.0.1
+ * Author: Jason Prendergast
+ * Author URI: https://github.com/JasonPrendergast
+ */
+class BFIGitHubPluginUpdater {
+ 
+    private $slug; // plugin slug
+    private $pluginData; // plugin data
+    private $username; // GitHub username
+    private $repo; // GitHub repo name
+    private $pluginFile; // __FILE__ of our plugin
+    private $githubAPIResult; // holds data from GitHub
+    private $accessToken; // GitHub private repo token
+ 
+    function __construct( $pluginFile, $gitHubUsername, $gitHubProjectName, $accessToken = '' ) {
+        add_filter( "pre_set_site_transient_update_plugins", array( $this, "setTransitent" ) );
+        add_filter( "plugins_api", array( $this, "setPluginInfo" ), 10, 3 );
+        add_filter( "upgrader_post_install", array( $this, "postInstall" ), 10, 3 );
+ 
+        $this->pluginFile = $pluginFile;
+        $this->username = $gitHubUsername;
+        $this->repo = $gitHubProjectName;
+        $this->accessToken = $accessToken;
+    }
+ 
+    // Get information regarding our plugin from WordPress
+    private function initPluginData() {
+        // code here
+    }
+ 
+    // Get information regarding our plugin from GitHub
+    private function getRepoReleaseInfo() {
+        // code here
+    }
+ 
+    // Push in plugin version information to get the update notification
+    public function setTransitent( $transient ) {
+        // code here
+        return $transient;
+    }
+ 
+    // Push in plugin version information to display in the details lightbox
+    public function setPluginInfo( $false, $action, $response ) {
+        // code ehre
+        return $response;
+    }
+ 
+    // Perform additional actions to successfully install our plugin
+    public function postInstall( $true, $hook_extra, $result ) {
+        // code here
+        return $result;
+    }
+}
